44b3dca63641e1058ac9e868ff0f5a28e5dd5a97
Rajesh Segu <rajesh.segu@gmail.com>
2014-11-26 01:25:12 -0800

    Update FBApp-Bridging-Header.h

--- FBApp/FBApp/FBApp-Bridging-Header.h
+++ FBApp/FBApp/FBApp-Bridging-Header.h
@@ -1,10 +1,4 @@
-//
-//  FBApp-Bridging-Header.h
-//  FBApp
-//
-//  Created by Rajesh Segu on 9/24/14.
-//  Copyright (c) 2014 500miles. All rights reserved.
-//
+
 
 #ifndef FBApp_FBApp_Bridging_Header_h
 #define FBApp_FBApp_Bridging_Header_h
